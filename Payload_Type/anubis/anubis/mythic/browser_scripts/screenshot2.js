a5f3f8824d42fb5d32c9e6c6eefd5bb8a1aa5ca1
Willian Oliveira - Escola Hack3r <71089186+wtechsec@users.noreply.github.com>
2025-08-26 09:13:11 -0300

    Create screenshot2.js

---
+++ Payload_Type/anubis/anubis/mythic/browser_scripts/screenshot2.js
@@ -0,0 +1,32 @@
+function(task, responses){
+    if(task.status.includes("error")){
+        const combined = responses.reduce((prev, cur) => {
+            return prev + cur;
+        }, "");
+        return {'plaintext': combined};
+    } else if(task.completed){
+        if(responses.length > 0){
+            let data = JSON.parse(responses[0].replace((new RegExp("'", 'g')), '"'));
+            return {"screenshot":[{
+                "agent_file_id": data["file_id"],
+                "variant": "contained",
+                "name": "View Screenshot 2"
+            }]};
+        } else {
+            return {"plaintext": "No data to display..."}
+        }
+    } else if(task.status === "processed"){
+        if(responses.length > 0){
+            let data = JSON.parse(responses[0]);
+            return {"screenshot":[{
+                "agent_file_id": data["file_id"],
+                "variant": "contained",
+                "name": "View Partial Screenshot 2"
+            }]};
+        }
+        return {"plaintext": "No data yet..."}
+    } else {
+        return {"plaintext": "No response yet from agent..."}
+    }
+}
+

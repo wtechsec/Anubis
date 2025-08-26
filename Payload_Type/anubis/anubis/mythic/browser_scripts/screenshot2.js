0c89cace7f544a5d10c55f34a3bccb1f0e2aa6dc
Willian Oliveira - Escola Hack3r <71089186+wtechsec@users.noreply.github.com>
2025-08-26 09:33:27 -0300

    Update screenshot2.js

--- Payload_Type/anubis/anubis/mythic/browser_scripts/screenshot2.js
+++ Payload_Type/anubis/anubis/mythic/browser_scripts/screenshot2.js
@@ -1,12 +1,10 @@
 function(task, responses){
     if(task.status.includes("error")){
-        const combined = responses.reduce((prev, cur) => {
-            return prev + cur;
-        }, "");
+        const combined = responses.reduce((prev, cur) => prev + cur, "");
         return {'plaintext': combined};
     } else if(task.completed){
         if(responses.length > 0){
-            let data = JSON.parse(responses[0].replace((new RegExp("'", 'g')), '"'));
+            let data = JSON.parse(responses[0].replace(/'/g, '"'));
             return {"screenshot":[{
                 "agent_file_id": data["file_id"],
                 "variant": "contained",

32dd40dc182377a00faa0722e440ae46a4583ab3
Willian Oliveira - Escola Hack3r <71089186+wtechsec@users.noreply.github.com>
2025-09-22 15:15:04 -0300

    Update rm.py

--- Payload_Type/anubis/anubis/mythic/agent_functions/rm.py
+++ Payload_Type/anubis/anubis/mythic/agent_functions/rm.py
@@ -1,59 +1,40 @@
-from mythic_container.MythicCommandBase import *
 import json
-from mythic_container.MythicRPC import *
-import sys
 
+def rm(self, task, data=None):
+    params = {"path": {"type": "str", "required": True, "description": "Path to remove (e.g., C:\\file.txt)"}}
+    if data and "path" in data:
+        path = data["path"]
+    else:
+        return "Path parameter is required"
+    task_options = {"command": "rm", "params": json.dumps({"path": path})}
+    response = self.create_tasking(task, task_options)
+    if response:
+        return f"Started task {task['id']} to remove {path}"
+    else:
+        return "Failed to start task to remove"
 
-class RmArguments(TaskArguments):
-    def __init__(self, command_line, **kwargs):
-        super().__init__(command_line, **kwargs)
-        self.args = [
-            CommandParameter(
-                name="path",
-                type=ParameterType.String,
-                parameter_group_info=[ParameterGroupInfo(
-                    required=True
-                )],
-                description="Read and output the content of a file",
-            )
-        ]
+def on_response(self, response, options=None):
+    if "removed_files" in response:
+        return f"Removed {response['removed_files'][0]['path']}"
+    return "No output from rm task"
 
-    async def parse_arguments(self):
-        if len(self.command_line) > 0:
-            if self.command_line[0] == "{":
-                temp_json = json.loads(self.command_line)
-                if "host" in temp_json:
-                    # this means we have tasking from the file browser rather than the popup UI
-                    # the apfell agent doesn't currently have the ability to do _remote_ listings, so we ignore it
-                    self.add_arg("path", temp_json["path"] + "/" + temp_json["file"])
-                else:
-                    self.add_arg("path", temp_json["path"])
-            else:
-                self.add_arg("path", self.command_line)
-        else:
-            raise ValueError("Missing arguments")
+command = {
+    "command": "rm",
+    "description": "Removes a file or directory.",
+    "author": "YourName",
+    "version": "1.0",
+    "parameters": [{"name": "path", "type": "str", "required": True, "description": "The full path to remove (e.g., C:\\file.txt)"}],
+    "dependencies": [],
+    "executors": ["default"],
+    "file_dependencies": [],
+    "supported_os": ["windows"],
+    "supported_ui_features": ["file_browser:remove"]
+}
 
-
-class RmCommand(CommandBase):
-    cmd = "rm"
-    needs_admin = False
-    help_cmd = "rm /path/to/file"
-    description = "Delete a file or folder"
-    version = 1
-    author = "@ajpc500"
-    attackmapping = [ "T1485" ]
-    supported_ui_features = ["file_browser:remove"]
-    argument_class = RmArguments
-    browser_script = BrowserScript(script_name="ls", author="@its_a_feature_")
-    attributes = CommandAttributes(
-        supported_python_versions=["Python 2.7", "Python 3.8"],
-        supported_os=[SupportedOS.MacOS, SupportedOS.Windows, SupportedOS.Linux ],
-    )
-
-    async def create_tasking(self, task: MythicTask) -> MythicTask:
-        task.display_params = "Deleting " + str(task.args.get_arg("path"))
-        return task
-
-    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
-        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
-        return resp
+def help(self):
+    return """
+    Command: rm
+    Description: Removes a file or directory.
+    Usage: rm path=<file_or_directory_path>
+    Example: rm path=C:\\file.txt
+    """

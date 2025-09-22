5e0bd2dd238de67897856652a3bac63154ee38c9
Willian Oliveira - Escola Hack3r <71089186+wtechsec@users.noreply.github.com>
2025-09-22 15:14:03 -0300

    Update ls.py

--- Payload_Type/anubis/anubis/mythic/agent_functions/ls.py
+++ Payload_Type/anubis/anubis/mythic/agent_functions/ls.py
@@ -1,62 +1,41 @@
-from mythic_container.MythicCommandBase import *
 import json
-from mythic_container.MythicRPC import *
-import sys
 
+def ls(self, task, data=None):
+    params = {"path": {"type": "str", "default": None, "description": "Path to list (e.g., C:\\Windows)"}}
+    if data and "path" in data:
+        path = data["path"]
+    else:
+        path = None
+    task_options = {"command": "ls", "params": json.dumps({"path": path}) if path else json.dumps({})}
+    response = self.create_tasking(task, task_options)
+    if response:
+        return f"Started task {task['id']} to list {path or 'current directory'}"
+    else:
+        return "Failed to start task to list"
 
-class LsArguments(TaskArguments):
-    def __init__(self, command_line, **kwargs):
-        super().__init__(command_line, **kwargs)
-        self.args = [
-            CommandParameter(
-                name="path",
-                type=ParameterType.String,
-                parameter_group_info=[ParameterGroupInfo(
-                    required=False
-                )],
-                description="Path of file or folder on the current system to list",
-            )
-        ]
+def on_response(self, response, options=None):
+    if "file_browser" in response:
+        return f"Listed {response.get('file_browser', {}).get('name', 'directory')}"
+    return "No output from ls task"
 
-    async def parse_arguments(self):
-        if len(self.command_line) > 0:
-            if self.command_line[0] == '{':
-                temp_json = json.loads(self.command_line)
-                if "host" in temp_json:
-                    self.add_arg("path", temp_json["path"] + "/" + temp_json["file"])
-                    self.add_arg("file_browser", True, type=ParameterType.Boolean)
-                else:
-                    self.add_arg("path", temp_json["path"])
-            else:
-                self.add_arg("path", self.command_line)
-        else:
-            self.add_arg("path", ".")
+command = {
+    "command": "ls",
+    "description": "Lists files and directories in the specified path.",
+    "author": "YourName",
+    "version": "1.0",
+    "parameters": [{"name": "path", "type": "str", "required": False, "description": "The directory path to list (e.g., C:\\Windows). If omitted, lists the current directory."}],
+    "dependencies": [],
+    "executors": ["default"],
+    "file_dependencies": [],
+    "supported_os": ["windows"],
+    "supported_ui_features": ["file_browser:list"]
+}
 
-class LsCommand(CommandBase):
-    cmd = "ls"
-    needs_admin = False
-    help_cmd = "ls [/path/to/file]"
-    description = "Get attributes about a file and display it to the user via API calls. No need for quotes and relative paths are fine"
-    version = 1
-    author = "@ajpc500"
-    attackmapping = ["T1083"]
-    supported_ui_features = ["file_browser:list"]
-    is_file_browse = True
-    argument_class = LsArguments
-    browser_script = BrowserScript(script_name="ls", author="@its_a_feature_", for_new_ui=True)
-    attributes = CommandAttributes(
-        supported_python_versions=["Python 2.7", "Python 3.8"],
-        supported_os=[SupportedOS.MacOS, SupportedOS.Windows, SupportedOS.Linux ],
-    )
-
-    async def create_tasking(self, task: MythicTask) -> MythicTask:
-        if task.args.has_arg("file_browser") and task.args.get_arg("file_browser"):
-            host = task.callback.host
-            task.display_params = host + ":" + task.args.get_arg("path")
-        else:
-            task.display_params = task.args.get_arg("path")
-        return task
-
-    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
-        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
-        return resp
+def help(self):
+    return """
+    Command: ls
+    Description: Lists files and directories in the specified path.
+    Usage: ls [path=<directory_path>]
+    Example: ls path=C:\\Windows
+             ls (lists current directory)
+    """
